51677ec1fb13aa112533d8e5f1a28d1807f51459
BerdibekUalov <uralov0212@gmail.com>
2023-05-31 14:39:35 +0500

    work with token file

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import getArgs from './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async token => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess("Token was save")
+  } catch (error) {
+    printError(error.message)
+  }
+}
 
 const startCli = () => {
   const args = getArgs(process.argv)
-  console.log(args);
   if(args.h) {
-    printHelp()
     // help
+    printHelp()
   }
   if(args.s) {
     // save city
   }
   if(args.t) {
     // save tokens
+    return saveToken(args.t)
   }
   // result
 }

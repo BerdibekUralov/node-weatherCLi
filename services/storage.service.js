99dc2377b33c970223f9e694a8cf1fe29a0c48c1
BerdibekUalov <uralov0212@gmail.com>
2023-06-01 11:34:42 +0500

    api.service.js working API and axios

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,40 +1,43 @@
-import os from 'os';
-import path from 'path';
+import os from 'os'
+import path from 'path'
 import fs from 'fs'
 
-const filePath= path.join(os.homedir(), 'weather-data.json');
+const filePath = path.join(os.homedir(), 'weather-data.json')
+
+const TOKEN_DICTIONARY = {
+	token: 'token',
+	city: 'city',
+}
 
 const saveKeyValue = async (key, value) => {
-  let data = {}
+	let data = {}
 
-  if(await isExist(filePath)) {
-    const file = await fs.promises.readFile(filePath)
-    data = JSON.parse(file)
-  }
-  data[key] = value
-  await fs.promises.writeFile(filePath, JSON.stringify(data))
+	if (await isExist(filePath)) {
+		const file = await fs.promises.readFile(filePath)
+		data = JSON.parse(file)
+	}
+
+	data[key] = value
+	await fs.promises.writeFile(filePath, JSON.stringify(data))
 }
 
 const getKeyValue = async key => {
-  if(await isExist(filePath)) {
-    const file = await fs.promises.readFile(filePath)
-    const data = JSON.parse(file)
-    return data(key)
-  }
+	if (await isExist(filePath)) {
+		const file = await fs.promises.readFile(filePath)
+		const data = JSON.parse(file)
+		return data[key]
+	}
 
-  return undefined
+	return undefined
 }
 
-const isExist = async (path) => {
-  try {
-    await fs.promises.stat(path)
-    return true
-  } catch (error) {
-    return false
-  }
+const isExist = async path => {
+	try {
+		await fs.promises.stat(path)
+		return true
+	} catch (error) {
+		return false
+	}
 }
 
-export {
-  saveKeyValue,
-  getKeyValue,
-}
+export {saveKeyValue, getKeyValue, TOKEN_DICTIONARY}
